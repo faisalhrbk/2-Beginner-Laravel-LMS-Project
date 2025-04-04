a911953b59fb933dff40535d73a392bf17e57bc0
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-04 16:47:08 +0500

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LMS App</title>
+</head>
+
+<body>
+    <nav>
+
+        <a href="/">HOME</a>
+        <a href="{{ route('teacher.index') }}">Start as Teacher</a>
+        <a href="{{ route('student.index') }}">Start as Student</a>
+
+    </nav>
+</body>
+
+</html>
