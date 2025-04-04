12789f3b6645a41458c4038facc744e85b803720
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-04 22:24:20 +0500

    created the schema for all users

--- database/migrations/2025_04_04_171517_create_students_table.php
+++ database/migrations/2025_04_04_171517_create_students_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->integer('age');
+            $table->text('address');
+            $table->integer('class');
+            $table->string('password');
         });
     }
 
